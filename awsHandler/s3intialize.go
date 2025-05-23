a7db60a8cec17a78fe03fc1d567e098739ed0adf
theMitocondria <dhruvmehta382@gmail.com>
2025-05-23 12:40:05 +0530

    Task: AWS Fix1

--- awsHandler/s3intialize.go
+++ awsHandler/s3intialize.go
@@ -10,7 +10,6 @@ import(
 var s3Client *s3.S3
 func InitializeAWS() error {
 	sess , err := session.NewSessionWithOptions(session.Options{
-		Profile : "default",
 		Config : aws.Config {
 			Region : aws.String("eu-north-1"),
 		},
@@ -19,6 +18,15 @@ func InitializeAWS() error {
 	if err != nil {
 		return fmt.Errorf("failed to create session: %v", err)
 	}
+
+	/ Debug: Check if credentials are loaded
+	creds := sess.Config.Credentials
+	val, err := creds.Get()
+	if err != nil {
+		return fmt.Errorf("unable to load AWS credentials: %v", err)
+	}
+	fmt.Printf("✅ AWS credentials loaded: %s\n", val.AccessKeyID)
+
 
 	s3Client = s3.New(sess)
 	return  nil
